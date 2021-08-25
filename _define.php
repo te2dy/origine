890b90133778205029bff1b981a089b2cb490a56
te2dy <52241628+te2dy@users.noreply.github.com>
2021-08-25 08:02:27 +0200

    Import des premiers fichiers

---
+++ _define.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */       "Origine",
+	/* Description*/ "",
+	/* Author */     "Teddy",
+	/* Version */    "alpha",
+	array(
+		'type' => 'theme'
+	)
+);
---
+++ _public.php
@@ -0,0 +1,6 @@
+<?php
+namespace themes\origine;
+
+if (!defined('DC_RC_PATH')) { return; }
+
+//\l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/public');
