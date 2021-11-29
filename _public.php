1042e2ed68865ede7ec5d31999bc201be506a68d
te2dy <52241628+te2dy@users.noreply.github.com>
2021-11-29 11:22:53 +0100

    Traduction du thème et ajout d’un bouton "Se souvenir de moi"

--- _public.php
+++ _public.php
@@ -3,4 +3,4 @@ namespace themes\origine;
 
 if (!defined('DC_RC_PATH')) { return; }
 
-//\l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/public');
+\l10n::set(dirname(__FILE__) . '/locales/' . $_lang . '/main');
