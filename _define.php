5fd5ca393712a36af204730aa98bb61959f987be
te2dy <52241628+te2dy@users.noreply.github.com>
2021-12-02 19:31:43 +0100

    Origine 0.7

--- _define.php
+++ _define.php
@@ -5,7 +5,7 @@ $this->registerModule(
   /* Name */       'Origine',
   /* Description*/ 'Un thème minimaliste.',
   /* Author */     'Teddy',
-  /* Version */    '0.6',
+  /* Version */    '0.7',
   array(
     'type'      => 'theme',
   )
