1caaa53fb3214ee4207798fc6ba498183e03b4e5
te2dy <52241628+te2dy@users.noreply.github.com>
2022-01-12 10:27:58 +0100

    Ajout de la prise en charge des billets sélectionnés

--- _public.php
+++ _public.php
@@ -5,10 +5,24 @@ if (!defined('DC_RC_PATH')) { return; }
 
 \l10n::set(dirname(__FILE__) . '/locales/' . $_lang . '/main');
 
+$core->tpl->addBlock('origineEntryIfSelected', [__NAMESPACE__ . '\tplOrigineTheme', 'origineEntryIfSelected']);
 $core->tpl->addValue('origineEntryLang', [__NAMESPACE__ . '\tplOrigineTheme', 'origineEntryLang']);
 
 class tplOrigineTheme
 {
+  public static function origineEntryIfSelected($attr, $content)
+  {
+    global $_ctx;
+
+    if (!$_ctx->posts->post_selected) {
+      $if_selected = '';
+    } else {
+      $if_selected = $content;
+    }
+
+    return $if_selected;
+  }
+
   /**
    * Displays a "lang" attribute and its value
    * when the language of the current post is different
