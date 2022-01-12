03028e847131791e8d0b6ac7ed7974d57d2a280f
te2dy <52241628+te2dy@users.noreply.github.com>
2022-01-12 12:00:59 +0100

    Correction de la description

--- _define.php
+++ _define.php
@@ -3,7 +3,7 @@ if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
   /* Name */       'Origine',
-  /* Description*/ 'Un thème minimaliste.',
+  /* Description*/ 'Un thème ultra minimaliste.',
   /* Author */     'Teddy',
   /* Version */    '1.0',
   array(
