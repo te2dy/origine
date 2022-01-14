cad16ece0d518ff84d0efd4b141de1fe994aab8c
te2dy <52241628+te2dy@users.noreply.github.com>
2022-01-14 11:57:33 +0100

    Correction d’une espace manquante

--- _define.php
+++ _define.php
@@ -2,11 +2,11 @@
 if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
-  /* Name */       'Origine',
-  /* Description*/ 'Un thème ultra minimaliste.',
-  /* Author */     'Teddy',
-  /* Version */    '1.1',
+  /* Name */        'Origine',
+  /* Description */ 'Un thème ultra minimaliste.',
+  /* Author */      'Teddy',
+  /* Version */     '1.1',
   array(
-    'type'      => 'theme',
+    'type' => 'theme',
   )
 );
