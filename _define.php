6962e2c4d4c0c4f9d0e07ffcff1ec1a70728ce27
te2dy <52241628+te2dy@users.noreply.github.com>
2022-01-20 12:04:53 +0100

    Origine 1.3.1

--- _define.php
+++ _define.php
@@ -5,7 +5,7 @@ $this->registerModule(
   /* Name */        'Origine',
   /* Description */ 'Un thème ultra minimaliste.',
   /* Author */      'Teddy',
-  /* Version */     '1.3',
+  /* Version */     '1.3.1',
   array(
     'type' => 'theme',
   )
