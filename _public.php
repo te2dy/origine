370412980b228da2047c3b929e2d49363093e33c
te2dy <52241628+te2dy@users.noreply.github.com>
2022-04-20 19:44:28 +0200

    Amélioration documentation et simplification code

--- _public.php
+++ _public.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * Origine, an ultra minimalist theme for Dotclear
- *
- * Note: all template tags found in html files
- * and whose name starts with {{tpl:origineConfig…
- * are handled by the plugin origineConfig.
+ * Origine, a minimalist theme for Dotclear
  *
  * @copyright Teddy
  * @copyright GPL-3.0
@@ -18,134 +14,45 @@ if (!defined('DC_RC_PATH')) {
 
 \l10n::set(dirname(__FILE__) . '/locales/' . $_lang . '/main');
 
-// Behaviors
 $core->addBehavior('publicHeadContent', [__NAMESPACE__ . '\tplOrigineTheme', 'publicHeadContent']);
-
-// Blocks
-$core->tpl->addBlock('origineCommentLinks', [__NAMESPACE__ . '\tplOrigineTheme', 'origineCommentLinks']);
-$core->tpl->addBlock('origineSidebar', [__NAMESPACE__ . '\tplOrigineTheme', 'origineSidebar']);
-$core->tpl->addBlock('origineFooter', [__NAMESPACE__ . '\tplOrigineTheme', 'origineFooter']);
-
-// Values
-$core->tpl->addValue('origineEntryIfSelected', [__NAMESPACE__ . '\tplOrigineTheme', 'origineEntryIfSelected']);
 $core->tpl->addValue('origineScreenReaderLinks', [__NAMESPACE__ . '\tplOrigineTheme', 'origineScreenReaderLinks']);
 $core->tpl->addValue('origineEntryLang', [__NAMESPACE__ . '\tplOrigineTheme', 'origineEntryLang']);
 $core->tpl->addValue('origineEntryPrintURL', [__NAMESPACE__ . '\tplOrigineTheme', 'origineEntryPrintURL']);
-$core->tpl->addValue('origineEntryCommentFeedLink', [__NAMESPACE__ . '\tplOrigineTheme', 'origineEntryCommentFeedLink']);
 $core->tpl->addValue('origineEntryPingURL', [__NAMESPACE__ . '\tplOrigineTheme', 'origineEntryPingURL']);
 
-/**
- * Template tags used in combination with origineConfig plugin.
- */
-
-// Values
+// Template tags used in combination with origineConfig plugin.
 $core->tpl->addValue('origineConfigActivationStatus', [__NAMESPACE__ . '\tplOrigineTheme', 'origineConfigActivationStatus']);
 $core->tpl->addValue('originePostListType', [__NAMESPACE__ . '\tplOrigineTheme', 'originePostListType']);
+$core->tpl->addValue('origineEntryIfSelected', [__NAMESPACE__ . '\tplOrigineTheme', 'origineEntryIfSelected']);
+$core->tpl->addValue('origineEntryCommentFeedLink', [__NAMESPACE__ . '\tplOrigineTheme', 'origineEntryCommentFeedLink']);
 $core->tpl->addValue('origineFooterCredits', [__NAMESPACE__ . '\tplOrigineTheme', 'origineFooterCredits']);
+$core->tpl->addBlock('origineCommentLinks', [__NAMESPACE__ . '\tplOrigineTheme', 'origineCommentLinks']);
+$core->tpl->addBlock('origineSidebar', [__NAMESPACE__ . '\tplOrigineTheme', 'origineSidebar']);
+$core->tpl->addBlock('origineFooter', [__NAMESPACE__ . '\tplOrigineTheme', 'origineFooter']);
 $core->tpl->addValue('origineInlineStyles', [__NAMESPACE__ . '\tplOrigineTheme', 'origineInlineStyles']);
 
 class tplOrigineTheme
 {
   /**
-   * Adds some meta tags in the <head> section
-   * depending on the blog settings.
+   * Adds meta tags in the <head> section depending on the blog settings.
    */
   public static function publicHeadContent()
   {
     global $core;
 
-    // Adds the name of the editor if set in the settings of Dotclear.
+    // Adds the name of the editor.
     if ($core->blog->settings->system->editor) {
       echo '<meta name="author" content="' . $core->blog->settings->system->editor . '" />' . "\n";
     }
 
-    // Adds the name of the copyright notice if set in the settings of Dotclear.
+    // Adds the content of the copyright notice.
     if ($core->blog->settings->system->copyright_notice) {
       echo '<meta name="copyright" content="' . $core->blog->settings->system->copyright_notice . '" />' . "\n";
     }
   }
 
   /**
-   * Displays a link to the comment feed and trackbacks,
-   * except if the plugin tells no.
-   */
-  public static function origineCommentLinks($attr, $content)
-  {
-    global $core;
-
-    $plugin_activated = self::origineConfigActivationStatus();
-
-    // If the plugin is installed and activated.
-    if ($plugin_activated === false
-      || ($plugin_activated === true && $core->blog->settings->origineConfig->origine_settings['content_comment_links'] === true)) {
-      return $content;
-    }
-  }
-
-  /**
-   * Displays the sidebar except if the plugin tells no.
-   */
-  public static function origineSidebar($attr, $content)
-  {
-    global $core;
-
-    $plugin_activated = self::origineConfigActivationStatus();
-
-    if ($plugin_activated === false
-      || ($plugin_activated === true && $core->blog->settings->origineConfig->origine_settings['widgets_enabled'] === true)
-    ) {
-      return $content;
-    }
-  }
-
-  /**
-   * Displays the footer except if the plugin tells no.
-   */
-  public static function origineFooter($attr, $content)
-  {
-    global $core;
-
-    $plugin_activated = self::origineConfigActivationStatus();
-
-    if ($plugin_activated === false
-      || ($plugin_activated === true && $core->blog->settings->origineConfig->origine_settings['footer_enabled'] === true)
-    ) {
-      return $content;
-    }
-  }
-
-  /**
-   * Displays a defined content when the current post is selected.
-   */
-  public static function origineEntryIfSelected()
-  {
-    global $core;
-
-    $plugin_activated = self::origineConfigActivationStatus();
-
-    if ($plugin_activated === false) {
-      $tpl = 'standard';
-    } else {
-      $tpl = $core->blog->settings->origineConfig->origine_settings['content_post_list_type'];
-    }
-
-    if ($tpl === 'standard' || $tpl === 'full') {
-      $label = __('Selected post');
-    } else {
-      $label = __('Selected');
-    }
-
-    return "<?php if (\$_ctx->posts->post_selected === '0') {"
-         . "echo '';"
-         . "} else {"
-         . "echo '<div class=\"label label-selected\">';"
-         . "echo '" . $label . "';"
-         . "echo '</div>';"
-         . "} ?>";
-  }
-
-  /**
-   * Displays navigation link for screen readers.
+   * Displays navigation links for screen readers.
    */
   public static function origineScreenReaderLinks()
   {
@@ -155,10 +62,8 @@ class tplOrigineTheme
     $links .= __('Skip to content');
     $links .= '</a>';
 
-    if ($core->plugins->moduleExists('simpleMenu') // simpleMenu exists.
-      && $core->blog->settings->system->simpleMenu_active // simpleMenu is activated.
-      && $core->blog->settings->system->simpleMenu // A menu is set.
-    ) {
+    // simpleMenu exists, is activated and a menu has been set.
+    if ($core->plugins->moduleExists('simpleMenu') && $core->blog->settings->system->simpleMenu_active && $core->blog->settings->system->simpleMenu) {
       $links .= '<a id="skip-menu" class="skip-links" href="#main-menu">';
       $links .= __('Skip to menu');
       $links .= '</a>';
@@ -174,11 +79,14 @@ class tplOrigineTheme
    */
   public static function origineEntryLang()
   {
-    return '<?php if ($_ctx->posts->post_lang !== $core->blog->settings->system->lang) { echo " lang=\"" . $_ctx->posts->post_lang . "\""; } ?>';
+    return '<?php if ($_ctx->posts->post_lang !== $core->blog->settings->system->lang) {
+      echo " lang=\"" . $_ctx->posts->post_lang . "\"";
+    } ?>';
   }
 
   /**
-   * Creates an URL without http or https to display on posts to print.
+   * Creates an URL of the current post without http or https
+   * to be displayed on printing only.
    */
   public static function origineEntryPrintURL()
   {
@@ -186,24 +94,7 @@ class tplOrigineTheme
   }
 
   /**
-   * Creates a link to the comment feed of current post.
-   */
-  public static function origineEntryCommentFeedLink($attr)
-  {
-    // Checks the type of the set feed.
-    $types = ['atom', 'rss2'];
-
-    if (isset($attr['type']) && in_array($attr['type'], $types)) {
-      $type = $attr['type'];
-    } else {
-      $type = 'atom';
-    }
-
-    return '<?php echo "<a href=\"" . $core->blog->url.$core->url->getURLFor("feed","' . $type . '") . "/comments/" . $_ctx->posts->post_id . "\" rel=\"nofollow\" type=\"application/' . $type . '+xml\">" . str_replace(array("http://", "https://"), "", $core->blog->url.$core->url->getURLFor("feed","' . $type . '") . "/comments/" . $_ctx->posts->post_id) . "</a>"; ?>';
-  }
-
-  /**
-   * Creates a link to the trackbacks of the current post.
+   * Creates a link to adds trackbacks for the current post.
    */
   public static function origineEntryPingURL()
   {
@@ -212,7 +103,7 @@ class tplOrigineTheme
     } ?>';
   }
 
-  // Template tags used in combination with origineConfig plugin:
+  // Template tags used in combination with origineConfig plugin.
 
   /**
    * Returns true if the plugin origineConfig
@@ -224,11 +115,7 @@ class tplOrigineTheme
   {
     global $core;
 
-    if ($core->plugins->moduleExists('origineConfig') === true
-      // Needed to support the new parameter origine_settings that contains all the plugin settings.
-      && version_compare('0.6.3', $core->plugins->moduleInfo('origineConfig', 'version'), '<')
-      && $core->blog->settings->origineConfig->origine_settings['activation'] === true
-    ) {
+    if ($core->plugins->moduleExists('origineConfig') === true && version_compare('0.6.3', $core->plugins->moduleInfo('origineConfig', 'version'), '<') && $core->blog->settings->origineConfig->origine_settings['activation'] === true) {
       return true;
     } else {
       return false;
@@ -255,7 +142,56 @@ class tplOrigineTheme
   }
 
   /**
-   * Displays credits except if the plugin tells no.
+   * Displays a text when the current post is selected.
+   */
+  public static function origineEntryIfSelected()
+  {
+    global $core;
+
+    $plugin_activated = self::origineConfigActivationStatus();
+
+    if ($plugin_activated === false) {
+      $tpl = 'standard';
+    } else {
+      $tpl = $core->blog->settings->origineConfig->origine_settings['content_post_list_type'];
+    }
+
+    if ($tpl === 'standard' || $tpl === 'full') {
+      $label = __('Selected post');
+    } else {
+      $label = __('Selected');
+    }
+
+    return '<?php if ($_ctx->posts->post_selected === "1") {
+      echo "<div class=\"label label-selected\">";
+      echo "' . $label . '";
+      echo "</div>";
+    } ?>';
+  }
+
+  /**
+   * Creates a link to the comment feed of current post.
+   */
+  public static function origineEntryCommentFeedLink($attr)
+  {
+    // Checks the type of the set feed.
+    $types = ['atom', 'rss2'];
+
+    if (isset($attr['type']) && in_array($attr['type'], $types)) {
+      $type = $attr['type'];
+    } else {
+      $type = 'atom';
+    }
+
+    return '<?php
+      $feed_url       = $core->blog->url . $core->url->getURLFor("feed", "' . $type . '");
+      $feed_url_short = str_replace(array("http://", "https://"), "", $feed_url);
+
+      echo "<a href=\"" . $feed_url . "/comments/" . $_ctx->posts->post_id . "\" rel=\"nofollow\" type=\"application/' . $type . '+xml\">" . $feed_url_short . "/comments/" . $_ctx->posts->post_id . "</a>"; ?>';
+  }
+
+  /**
+   * Displays credits in the footer.
    */
   public static function origineFooterCredits()
   {
@@ -264,9 +200,7 @@ class tplOrigineTheme
     $plugin_activated = self::origineConfigActivationStatus();
     $the_footer       = '';
 
-    if ($plugin_activated === false
-      || ($plugin_activated === true && $core->blog->settings->origineConfig->origine_settings['footer_credits'] === true)
-    ) {
+    if ($plugin_activated === false || ($plugin_activated === true && $core->blog->settings->origineConfig->origine_settings['footer_credits'] === true)) {
       $the_footer .= '<div class="widget" id="site-footer-ad">';
 
       $url_dotclear  = __('https://dotclear.org/');
@@ -295,13 +229,55 @@ class tplOrigineTheme
   }
 
   /**
+   * Displays a link to the comment feed and trackbacks.
+   */
+  public static function origineCommentLinks($attr, $content)
+  {
+    global $core;
+
+    $plugin_activated = self::origineConfigActivationStatus();
+
+    if ($plugin_activated === false || ($plugin_activated === true && $core->blog->settings->origineConfig->origine_settings['content_comment_links'] === true)) {
+      return $content;
+    }
+  }
+
+  /**
+   * Displays the sidebar.
+   */
+  public static function origineSidebar($attr, $content)
+  {
+    global $core;
+
+    $plugin_activated = self::origineConfigActivationStatus();
+
+    if ($plugin_activated === false || ($plugin_activated === true && $core->blog->settings->origineConfig->origine_settings['widgets_enabled'] === true)) {
+      return $content;
+    }
+  }
+
+  /**
+   * Displays the footer.
+   */
+  public static function origineFooter($attr, $content)
+  {
+    global $core;
+
+    $plugin_activated = self::origineConfigActivationStatus();
+
+    if ($plugin_activated === false || ($plugin_activated === true && $core->blog->settings->origineConfig->origine_settings['footer_enabled'] === true)) {
+      return $content;
+    }
+  }
+
+  /**
    * Adds styles in the head section.
    *
    * If the plugin origineConfig is not installed nor activated,
    * it will load default styles; otherwise, it will load
    * the custom styles set in the plugin page.
    *
-   * @see styles.css
+   * @see origine/styles.css
    */
   public static function origineInlineStyles()
   {
